ff2db4936566f8f06c8ba32bffb4e68586a92f50
Stefan456789 <stefanfarbe@gmail.com>
2024-02-25 15:26:02 +0100

    Open links in default browser

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,11 +1,22 @@
+<script setup lang="ts">
+import { open } from '@tauri-apps/api/shell'
+function openWindow (link: string) {
+  open(link)
+}
+</script>
 <template>
   <v-footer :elevation="2">
-    <v-row no-gutters>
+    <v-row no-gutters class="items-center">
       <v-btn :elevation="0">
         Contact
       </v-btn>
       <v-spacer />
-      <v-btn :elevation="0" href="https://opensource.org/license/mit/" target="_blank">
+
+      <v-btn :elevation="0" @click="openWindow('https://github.com/Stefan456789')">
+        by Stefan Wiesinger
+      </v-btn>
+      <v-spacer />
+      <v-btn :elevation="0" @click="openWindow('https://opensource.org/license/mit/')">
         MIT License
       </v-btn>
     </v-row>
